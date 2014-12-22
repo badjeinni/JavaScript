36225cbeb7b7ccebd9b543e37ae664d6fdc383bf
Djeinaba BA <djeinababa@MacBook-Pro-de-Djeinaba.local>
2014-12-22 21:35:20 +0100

    Initial Commit

---
+++ JavaScript/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JavaScript
+//
+//  Created by Djeinaba BA on 22/12/2014.
+//  Copyright (c) 2014 Djeinaba BA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JavaScript/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JavaScript
+//
+//  Created by Djeinaba BA on 22/12/2014.
+//  Copyright (c) 2014 Djeinaba BA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
